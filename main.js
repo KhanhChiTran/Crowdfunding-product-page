afcae1aa7c1990b29248ef1b7b3646fde7421567
ChiTran <Khanhchi.tran13051@gmail.com>
2021-03-11 23:37:53 +0100

    add success pop-up

--- main.js
+++ main.js
@@ -79,3 +79,47 @@ const bookmarkIcon = document.querySelector(".bookmark");
 bookmarkIcon.onclick = () => {
   console.log("clicking ....");
 };
+
+const modalBtn2 = document.querySelector(".modal-btn2");
+const ctaCard2 = document.querySelector(".cta__card2");
+const modalCard2 = document.querySelector(".modal-card2");
+modalBtn2.onclick = () => {
+  modalBtn2.style.display = "none";
+  ctaCard2.style.display = "block";
+  modalCard2.style.border = "3px solid hsl(174, 86%, 45%)";
+  modalCheckbox3.style.background = "hsl(174, 86%, 45%)";
+};
+const modalBtn1 = document.querySelector(".modal-btn1");
+const ctaCard1 = document.querySelector(".cta__card1");
+const modalCard1 = document.querySelector(".modal-card1");
+modalBtn1.onclick = () => {
+  modalBtn1.style.display = "none";
+  ctaCard1.style.display = "block";
+  modalCard1.style.border = "3px solid hsl(174, 86%, 45%)";
+  modalCheckbox2.style.background = "hsl(174, 86%, 45%)";
+};
+
+const successModalCard = document.querySelector(".success-modal-card");
+const continueBtn1 = document.querySelector(".continue1");
+const continueBtn2 = document.querySelector(".continue2");
+const successBtn = document.querySelector(".success-btn");
+continueBtn1.onclick = () => {
+  successModalCard.style.display = "block";
+  modalLayout.style.display = "none";
+  mainCards.style.display = "block";
+  if (window.scrollY) {
+    window.scroll(0, 0); // reset the scroll position to the top left of the document.
+  }
+};
+continueBtn2.onclick = () => {
+  successModalCard.style.display = "block";
+  modalLayout.style.display = "none";
+  mainCards.style.display = "block";
+  if (window.scrollY) {
+    window.scroll(0, 0); // reset the scroll position to the top left of the document.
+  }
+};
+successBtn.onclick = () => {
+  successModalCard.style.display = "none";
+  modal.style.display = "none";
+};
